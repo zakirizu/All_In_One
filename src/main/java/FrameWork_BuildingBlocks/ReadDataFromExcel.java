8ddf380ca372c8d32146bc54756428b524279186
Farooq <>
2022-12-28 12:56:01 +0530

    Added new Actions

---
+++ src/main/java/FrameWork_BuildingBlocks/ReadDataFromExcel.java
@@ -0,0 +1,10 @@
+package FrameWork_BuildingBlocks;
+
+public class ReadDataFromExcel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/FrameWork_BuildingBlocks/WriteDataToExcel.java
@@ -0,0 +1,14 @@
+package FrameWork_BuildingBlocks;
+
+public class WriteDataToExcel {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+
+}
