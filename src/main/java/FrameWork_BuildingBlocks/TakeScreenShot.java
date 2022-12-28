97c0bb04c7fe85327654b56f5c66b1abccc0c0a4
Zakir <79053961+zakirizu@users.noreply.github.com>
2022-12-28 18:15:09 +0530

    Takes Screen Shot Module

---
+++ src/main/java/FrameWork_BuildingBlocks/TakeScreenShot.java
@@ -0,0 +1,42 @@
+package FrameWork_BuildingBlocks;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TakeScreenShot {
+	static WebDriver driver;
+	static String path = System.getProperty("user.dir")+"\\Resources\\ScreenShots\\test.png";
+	
+	
+	
+	
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Work\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.browserstack.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	//Convert WebDriver Object to TakeScreenShot
+		Thread.sleep(5000);
+		TakesScreenshot  scrShot  = ((TakesScreenshot) driver);
+	//Call  getScreenshotAs method to create Image file	
+		File srFile = scrShot.getScreenshotAs(OutputType.FILE);
+		File destFile =  new File(path);
+		FileUtils.copyFile(srFile, destFile);
+		driver.quit();
+
+
+	}
+
+}
