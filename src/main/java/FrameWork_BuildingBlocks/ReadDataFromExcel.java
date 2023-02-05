59f846a4456f76cc63f9428a326bfde3dfedd163
Zakir <79053961+zakirizu@users.noreply.github.com>
2023-02-05 08:03:09 +0530

    Update ReadDataFromExcel.java
    update

--- src/main/java/FrameWork_BuildingBlocks/ReadDataFromExcel.java
+++ src/main/java/FrameWork_BuildingBlocks/ReadDataFromExcel.java
@@ -28,30 +28,18 @@ public class ReadDataFromExcel {
 		try {
 		 hmap = new HashMap<String, String>();
 		String path = System.getProperty("user.dir")+"\\Resources\\TestData.xlsx";
+		
 		FileInputStream fis = new FileInputStream(path);		
 		XSSFWorkbook workbook= new XSSFWorkbook(fis);		
-		XSSFSheet sheet = workbook.getSheet(sheetName);			
+		XSSFSheet sheet = workbook.getSheet(sheetName);	
+		
 		int lastRowCount = sheet.getLastRowNum();		
 		System.out.println(" Last Row Count is "+lastRowCount);		
 		for(int i=0; i<lastRowCount;i++)
 		{
 		String temp = sheet.getRow(i).getCell(0).getStringCellValue();
 		if(temp.equalsIgnoreCase(tcID))
-		{
-			XSSFRow targetKeyRow = sheet.getRow(i);
-			XSSFRow targetValueRow = sheet.getRow(i+1);
-			int lastColumn = targetKeyRow.getLastCellNum();
-			System.out.println(" Last Column Count is "+lastColumn);
-			
-			for(int j=1;j<lastColumn-1;j++)
-			{
-				String k = targetKeyRow.getCell(j).getStringCellValue();
-				String v = targetValueRow.getCell(j).getStringCellValue();
-				hmap.put(k,v);
-				//System.out.println(k);
-				//System.out.println(v);
-			}
-			break;}
+		{ }
 	
 		}
 		
