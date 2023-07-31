26e4a440b5d482c59ad9fa3be73b4a6acdcbe68a
Zakir <79053961+zakirizu@users.noreply.github.com>
2023-07-31 08:05:57 +0530

    Create JavaRemoveDuplicates.java

---
+++ src/main/java/java_Interview/JavaRemoveDuplicates.java
@@ -0,0 +1,26 @@
+package java_Interview;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class JavaRemoveDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1,2,2,3,4,4,2,8,1};
+        HashSet<Integer> hset = new  HashSet<Integer>();
+        for(int i=0;i<arr.length; i++)
+        {
+            hset.add(arr[i]);
+        }
+        
+        Iterator ite = hset.iterator();
+        while(ite.hasNext())
+        {
+            System.out.println(ite.next());
+        }
+
+	}
+
+}
