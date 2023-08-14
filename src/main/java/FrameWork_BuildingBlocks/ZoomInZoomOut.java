e530ce69ed01e74c5c3e5d142922b30494273169
Zakir <79053961+zakirizu@users.noreply.github.com>
2023-08-14 22:13:25 +0530

    Update

---
+++ src/main/java/FrameWork_BuildingBlocks/ZoomInZoomOut.java
@@ -0,0 +1,76 @@
+package FrameWork_BuildingBlocks;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.AWTException;
+import java.awt.Robot;	
+import java.awt.event.KeyEvent;	
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ZoomInZoomOut {
+
+
+	public static void main(String[] args) throws AWTException, InterruptedException {
+	
+		ZoomIn();
+		ZoomOut();
+
+	}
+
+	private static  void ZoomIn() throws AWTException, InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		 chromeOptions.addArguments("--remote-allow-origins=*");
+		 WebDriver driver = new ChromeDriver(chromeOptions);
+				
+	//	WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.get("https://www.softwaretestingmaterial.com/");
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);		
+		robot.keyPress(KeyEvent.VK_ADD);
+		robot.keyRelease(KeyEvent.VK_ADD);
+		robot.keyPress(KeyEvent.VK_ADD);
+		robot.keyRelease(KeyEvent.VK_ADD);
+		robot.keyPress(KeyEvent.VK_ADD);
+		robot.keyRelease(KeyEvent.VK_ADD);
+		Thread.sleep(5000);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+	}
+	
+	private static void ZoomOut() throws AWTException, InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		 chromeOptions.addArguments("--remote-allow-origins=*");
+		 WebDriver driver = new ChromeDriver(chromeOptions);
+				
+	//	WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		driver.get("https://www.softwaretestingmaterial.com/");
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_SUBTRACT);
+		robot.keyRelease(KeyEvent.VK_CONTROL);	
+		Thread.sleep(5000);
+		
+	}
+
+
+
+}
