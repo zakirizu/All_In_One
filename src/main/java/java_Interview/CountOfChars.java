04787b09df58f20a03fd295445d58328997e923b
Zakir <79053961+zakirizu@users.noreply.github.com>
2024-01-03 14:09:40 +0530

    Update
    Description

--- src/main/java/java_Interview/CountOfChars.java
+++ src/main/java/java_Interview/CountOfChars.java
@@ -10,15 +10,11 @@ public class CountOfChars {
 
 	public static void main(String[] args) {
 	String given  	= "APPLE";
-	String str		= (given.replace(" ", "")).toLowerCase();
-	
-	
+	String str		= (given.replace(" ", "")).toLowerCase();	
 	/**************************LOGIC***************************
 	 *  Map Keys - Unique, can contain one null value
 	 *  values can be duplicate
-	 * ********************************************************
-	 */
-	
+	 * ******************************************************** */
 	//Declaring HashMap
 	HashMap<Character, Integer> hmap = new HashMap<Character,Integer>();
 	for(int i =0; i<str.length(); i++)
@@ -31,10 +27,9 @@ public class CountOfChars {
 		else
 		{
 		//if it is already present then get the value of that key and then add 1 to that value as it has repeated for the second time
-		int value = hmap.get(key);
+		int value = hmap.get(key); 
 		hmap.put(key, value+1);		
 		}
-		
 	}
 	
 	Iterator<Character> keyiter = hmap.keySet().iterator();
