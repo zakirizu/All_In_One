8a410c431bec5a4086d0a094ce2c66534255fce0
Aman <zakir.13.2011@gmail.com>
2025-11-04 07:33:27 +0530

    Update Engine.java

--- src/main/java/clasess/Engine.java
+++ src/main/java/clasess/Engine.java
@@ -1,29 +1,10 @@
 package clasess;
 
-abstract class EngineRules_Govt {
-
-	public static void main(String[] args) {
-		
-	}
-	
-	public abstract void startEngine();
-	
-	public abstract void accelarator();
-	
-	
-	public void SoundHorn() {
-		System.out.println("Click on Steering for Sounding Horn");
-	}
-	
-	public void FourTyres() {
-		System.out.println("Four Tyres are there to support the Vehicle");
-	}
-	
-	public void SafetyBags() {
-		System.out.println("Safety Bags are there to protect the Passengers during Accident");
-	}
-
-
-
+interface EngineRules_Govt {
+	void startEngine();
+	void accelarator();
+	void SoundHorn();
+	void FourTyres();
+	void SafetyBags();
 
 }
